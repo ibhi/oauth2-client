353c5ed6f0291b6d3d1c215405bebf366f8af9dc
ibhi <ibrahimin2004@gmail.com>
2017-10-10 22:25:14 +0530

    chore: All the Oauth parameters changed to be read from application properties

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 ## Demo Project for Java OAuth 2.0 Client using Apache OLTU
 
 ### application.properties
-Add application.properties file under resources folder with the following two fields
-* oauth2.clientId=your-client-id
-* oauth2.clientSecret=your-client-secret
+Add application.properties file under resources folder with the following fields
+
+```
+oauth.clientId=<your-client-id>
+oauth.clientSecret=y<our-client-secret>
+oauth.redirectUri=http://localhost:8080/redirect
+oauth.authorizationLocation=https://accounts.google.com/o/oauth2/v2/auth
+oauth.tokenLocation=https://www.googleapis.com/oauth2/v4/token
+oauth.scope=profile openid
+```
--- src/main/java/in/noobish/oauth2/controllers/OauthController.java
+++ src/main/java/in/noobish/oauth2/controllers/OauthController.java
@@ -12,7 +12,6 @@ import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.client.OAuthClient;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
-import org.apache.oltu.oauth2.common.OAuthProviderType;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
@@ -20,44 +19,54 @@ import org.apache.oltu.oauth2.common.message.types.GrantType;
 @RestController
 public class OauthController {
 
-    @Value("${oauth2.clientId}")
+    @Value("${oauth.clientId}")
     private String clientId;
 
-    @Value("${oauth2.clientSecret}")
+    @Value("${oauth.clientSecret}")
     private String clientSecret;
 
+    @Value("${oauth.redirectUri}")
+    private String redirectUri;
+
+    @Value("${oauth.authorizationLocation}")
+    private String authorizationLocation;
+
+    @Value("${oauth.tokenLocation}")
+    private String tokenLocation;
+
+    @Value("${oauth.scope}")
+    private String scope;
+
+    private final String RESPONSE_TYPE = "code";
+
     @RequestMapping("/")
-    public void getAuthCode(HttpServletResponse response) throws OAuthSystemException, IOException {
+    public void getAuthCode(final HttpServletResponse response) throws OAuthSystemException, IOException {
 
         OAuthClientRequest request = OAuthClientRequest
-                    .authorizationProvider(OAuthProviderType.GOOGLE)
-                    .setClientId(clientId)
-                    .setScope("profile openid")
-                    .setRedirectURI("http://localhost:8080/redirect")
-                    .setResponseType("code")
-                    .buildQueryMessage();
+                .authorizationLocation(authorizationLocation)
+                .setClientId(clientId)
+                .setScope(scope)
+                .setRedirectURI(redirectUri)
+                .setResponseType(RESPONSE_TYPE)
+                .buildQueryMessage();
 
-            response.sendRedirect(request.getLocationUri());
+        response.sendRedirect(request.getLocationUri());
     }
 
     @RequestMapping("/redirect")
     public String getAccessToken(@RequestParam("code") String authCode) throws OAuthSystemException, OAuthProblemException {
 
         OAuthClientRequest request = OAuthClientRequest
-                .tokenProvider(OAuthProviderType.GOOGLE)
+                .tokenLocation(tokenLocation)
                 .setGrantType(GrantType.AUTHORIZATION_CODE)
                 .setClientId(clientId)
                 .setClientSecret(clientSecret)
-                .setRedirectURI("http://localhost:8080/redirect")
+                .setRedirectURI(redirectUri)
                 .setCode(authCode)
                 .buildBodyMessage();
 
         OAuthClient oAuthClient = new OAuthClient(new OAuthOkHttpClient());
 
-        //Facebook is not fully compatible with OAuth 2.0 draft 10, access token response is
-        //application/x-www-form-urlencoded, not json encoded so we use dedicated response class for that
-        //Own response class is an easy way to deal with oauth providers that introduce modifications to
-        //OAuth specification
         OAuthJSONAccessTokenResponse oAuthResponse = oAuthClient.accessToken(request, OAuthJSONAccessTokenResponse.class);
 
         System.out.println(
